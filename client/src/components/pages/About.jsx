f7aadf7d5f8a5b9a28aabb86a78d4c3b1be1e346
Kiril Mankovskyi <200412952@student.georgianc.on.ca>
2020-07-31 17:06:56 -0400

    done

---
+++ client/src/components/pages/About.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+function About() {
+    return (
+        <Container>
+            <header class="jumbotron">
+                <h1>All About Me</h1>
+            </header>
+
+            <div>
+                <p>The autobiography of Shaun McKinnon</p>
+            </div>
+        </Container>
+    );
+}
+
+export default About;
